66c155ab8f4752e7d8a30a82eb5a60a92d7611cb
Ben Schutte <schutte.bj@gmail.com>
2025-05-28 21:15:55 +0200

    Fix CORS settings reference - use BACKEND_CORS_ORIGINS instead of undefined CORS_ORIGINS

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ app = FastAPI(
 # Set all CORS enabled origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
